d0b45521230b15d2a4a08b6c188418559a1585f2
m0t44_ <gabrielmmoreto@gmail.com>
2023-11-16 16:43:18 -0300

    api-render

--- src/API/apiLocal/api.js
+++ src/API/apiLocal/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const apiLocal = axios.create({
-    baseURL: 'http://localhost:3333'
+    // baseURL: 'http://localhost:3333'
+    baseURL: 'https://node-deploy-senac-6mzx.onrender.com'
 })
 
 export default apiLocal
